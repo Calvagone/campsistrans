dd65553011587600e8cfe9a7e9acdbd7aa4e2997
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-02-17 16:08:36 +0100

    ADVAN4 non-regression tests.

--- tests/testthat/testSubroutine.R
+++ tests/testthat/testSubroutine.R
@@ -3,13 +3,14 @@ library(ggplot2)
 
 context("Non-regression test on subroutine conversion")
 testFolder <<- ""
+#testFolder <<- "C:/prj/pmxtran/tests/testthat/"
 
 modelPath <- function(advan, trans) {
   return(paste0(testFolder, "models/subroutine/advan", advan, "_trans", trans, ".mod"))
 }
 
 toTmpFile <- function(code, advan, trans) {
-  fileConn <- file(paste0(testFolder, "models/subroutine/non_regression/advan", advan, "_trans", trans, "_tmp.txt"))
+  fileConn <- file(paste0(testFolder, "models/subroutine/non_regression/advan", advan, "_trans", trans, ".txt"))
   writeLines(code, fileConn)
   close(fileConn)
 }
@@ -21,6 +22,8 @@ loadModel <- function(advan, trans) {
 generateModel <- function(advan, trans) {
   model <- importNONMEM(modelPath(advan=advan, trans=trans))
   code <- toRxODE(model)
+  toTmpFile(code, advan, trans)
+  return(code)
 }
 
 test_that("ADVAN3 TRANS4", {
@@ -30,10 +33,34 @@ test_that("ADVAN3 TRANS4", {
   expect_equal(code, loadModel(advan, trans))
 })
 
+test_that("ADVAN4 TRANS1", {
+  # TODO: F=A_CENTRAL is wrong
+  advan <- 4
+  trans <- 1
+  code <- generateModel(advan, trans)
+  expect_equal(code, loadModel(advan, trans))
+})
+
+test_that("ADVAN4 TRANS3", {
+  # TODO: F=A_CENTRAL is wrong
+  advan <- 4
+  trans <- 3
+  code <- generateModel(advan, trans)
+  expect_equal(code, loadModel(advan, trans))
+})
+
 test_that("ADVAN4 TRANS4", {
   # TODO: F=A_CENTRAL is wrong
   advan <- 4
   trans <- 4
+  code <- generateModel(advan, trans)
+  expect_equal(code, loadModel(advan, trans))
+})
+
+test_that("ADVAN4 TRANS5", {
+  # TODO: F=A_CENTRAL is wrong
+  advan <- 4
+  trans <- 5
   code <- generateModel(advan, trans)
   expect_equal(code, loadModel(advan, trans))
 })
