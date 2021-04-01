c1904d62501239cf89e986318ee1b33e36b6fe6a
Nicolas Luyckx <nicolas.luyckx@calvagone.com>
2021-04-01 09:42:44 +0200

    Instructions for Python.

--- tests/testthat/testUtils.R
+++ tests/testthat/testUtils.R
@@ -3,6 +3,12 @@
 # roxygen2::roxygenise()
 # setwd("C:/prj/pmxtran/tests/")
 # testFolder <<- "C:/prj/pmxtran/tests/testthat/"
+
+
+# Python environment
+# Add line in Rprofile.site: Sys.setenv("RETICULATE_PYTHON" = "C:/PsN-5.0.0/python/python-3.7.7.amd64/")
+
+# This way, below code is not needed
 # reticulate::use_python("C:/PsN-5.0.0/python/python-3.7.7.amd64/python.exe", required=TRUE)
 # reticulate::py_config()
 # version <- pharmpy["__version__"]
