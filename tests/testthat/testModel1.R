892f531db38c6ce10b96e9b31abf3c6cad2d8c66
luyckxn <nicolas.luyckx@calvagone.com>
2024-01-22 15:38:13 +0100

    #31 Refactor testModel1.R

--- tests/testthat/testModel1.R
+++ tests/testthat/testModel1.R
@@ -6,17 +6,17 @@ context("Tests on custom model 1")
 testFolder <<- ""
 overwriteNonRegressionFiles <- FALSE
 
-modelPath <- function(modelName) {
-  return(paste0(testFolder, "custom_models/", modelName))
+modelPath <- function(modelDir, modelName) {
+  return(paste0(testFolder, "custom_models/", modelDir, "/", modelName))
 }
 
 nonRegressionFolderPath <- function(regFolder) {
   return(paste0(testFolder, "non_regression/custom/", regFolder, "/"))
 }
 
-generateModel <- function(modelName, regFolder, mapping) {
+generateModel <- function(modelDir, modelName, regFolder, mapping) {
   # Import your NONMEM model using pharmpy
-  object <- importNONMEM(modelPath(modelName), mapping)
+  object <- importNONMEM(modelPath(modelDir, modelName), mapping)
   
   model <- object %>% export(dest="campsis")
   if (overwriteNonRegressionFiles) {
@@ -26,25 +26,28 @@ generateModel <- function(modelName, regFolder, mapping) {
 }
 
 test_that("Model 1", {
+  modelDir <- "model1"
   modelName <- "model1.mod"
-  regFolder <- "model1"
+  regFolder <- modelDir
   
+
   # Also map OMEGA names because 'SAME' OMEGAS related to IOV not listed in Pharmpy parameter set
   mapping <- mapping(theta=c("CL"=1, "V2"=2, "Q3"=3, "V3"=4, "KA"=5, "F1"=6, "W"=7, "NFOOD"=8, "LAMB"=9, "IMAX"=10, "CLBW"=11, "VDBW"=12, "CL99"=13),
                      omega=1:24,
                      sigma=c("ADD"=1))
   
-  model <- generateModel(modelName=modelName, regFolder=regFolder, mapping=mapping)
-  expect_equal(model, campsismod::read.campsis(nonRegressionFolderPath(regFolder)))
+  model <- generateModel(modelDir=modelDir, modelName=modelName, regFolder=modelDir, mapping=mapping)
+  expect_equal(model, campsismod::read.campsis(nonRegressionFolderPath(modelDir)))
 })
 
 test_that("Model 1 (auto-mapping)", {
+  modelDir <- "model1"
   modelName <- "model1.mod"
   regFolder <- "model1_auto_mapping"
   
   # Also map OMEGA names because 'SAME' OMEGAS related to IOV not listed in Pharmpy parameter set
   mapping <- mapping(omega=1:24, sigma=c("ADD"=1), auto=TRUE)
   
-  model <- generateModel(modelName=modelName, regFolder=regFolder, mapping=mapping)
+  model <- generateModel(modelDir=modelDir, modelName=modelName, regFolder=regFolder, mapping=mapping)
   expect_equal(model, campsismod::read.campsis(nonRegressionFolderPath(regFolder)))
 })
