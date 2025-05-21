0f45a501421c6caf04a3d36f21e302c83704dfbe
luyckxn <nicolas.luyckx@calvagone.com>
2025-05-21 14:25:53 +0200

    #77 Fix  R CMD Check.

--- tests/testthat/testAutoMapping.R
+++ tests/testthat/testAutoMapping.R
@@ -3,19 +3,14 @@ library(campsismod)
 library(ggplot2)
 
 context("Testing the auto mapping functionnality")
-
-testFolder <<- ""
+testFolder <-  file.path(getwd(), test_path())
 
 advanFilename <- function(advan, trans, ext=".txt") {
   return(paste0("advan", advan, "_trans", trans, ext))
 }
 
-nonRegressionFolderPath <- function(advan, trans) {
-  return(paste0(testFolder, "non_regression/subroutine/", advanFilename(advan, trans, ext=""), "/"))
-}
-
-loadAdvanNonRegressionFile <- function(advan, trans) {
-  return(read.campsis(nonRegressionFolderPath(advan, trans)))
+nonRegressionPharmpyPath <- function(advan, trans) {
+  return(file.path(testFolder, "non_regression", "subroutine", "pharmpy", advanFilename(advan, trans, ext="")))
 }
 
 generateModel <- function(advan, trans, mapping=NULL) {
@@ -31,7 +26,7 @@ test_that("ADVAN1 TRANS1 (auto-mapping)", {
   trans <- 1
   mapping <- mapping(sigma=c(PROP=1), auto=TRUE)
   model <- generateModel(advan, trans, mapping)
-  expect_equal(model, loadAdvanNonRegressionFile(advan, trans))
+  expect_equal(model, read.campsis(nonRegressionPharmpyPath(advan, trans)))
 })
 
 test_that("ADVAN4 TRANS3 (auto-mapping)", {
@@ -40,7 +35,7 @@ test_that("ADVAN4 TRANS3 (auto-mapping)", {
   mapping <- mapping(auto=TRUE)
   model <- generateModel(advan, trans, mapping)
   
-  expected_model <- loadAdvanNonRegressionFile(advan, trans)
+  expected_model <- read.campsis(nonRegressionPharmpyPath(advan, trans))
   expected_model@parameters@list[[11]]@name <- "RSV"
   expected_model <- expected_model %>% replaceAll("EPS_PROP", "EPS_RSV")
   
