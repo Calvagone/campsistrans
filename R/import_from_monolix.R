57253a76b0b02928a5a056b9342811a9940eec1a
luyckxn <nicolas.luyckx@calvagone.com>
2025-05-22 13:58:05 +0200

    #74 Rework function importMonolix.

--- R/import_from_monolix.R
+++ R/import_from_monolix.R
@@ -17,15 +17,14 @@ copyAndRename <- function(file, tempDir, newName) {
 #' @param mlxtranFile to mlxtran file
 #' @param modelFile path to model file, optional
 #' @param parametersFile path to estimated parameters file, optional
+#' @param covFile path to covariance estimates file, optional
 #' @param digits number of significant digits, integer or NULL to disable rounding
 #' @return a functional Campsis model
 #' @export
 #' @importFrom digest sha1
 #' @importFrom monolix2rx mlxtran monolix2rx
 #' 
-importMonolix <- function(mlxtranFile, modelFile=NULL, parametersFile=NULL, digits=NULL) {
-  # browser()
-  
+importMonolix <- function(mlxtranFile, modelFile=NULL, parametersFile=NULL, covFile=NULL, digits=NULL) {
   # Create temporary directory
   tempDir <- tempdir()
   hash <- substr(digest::sha1(paste0(Sys.time(), mlxtranFile)), 1, 10)
@@ -33,27 +32,14 @@ importMonolix <- function(mlxtranFile, modelFile=NULL, parametersFile=NULL, digi
   if (!dir.exists(tempDir)) {
     dir.create(tempDir)
   }
-
-  # Create export directory
-  exportDir <- file.path(tempDir, "export")
-  if (!dir.exists(exportDir)) {
-    dir.create(exportDir)
-  }
+  
   print(gsub(pattern="\\\\", replacement="/", x=normalizePath(tempDir)))
   
+  # Is there a external model?
   noExternalModel <- is.null(modelFile) || !file.exists(modelFile)
   
   # Copy mlxtran file to temporary directory
   mlxtran <- copyAndRename(file=mlxtranFile, tempDir=tempDir, newName="project.mlxtran")
-  
-  # Copy model file to temporary directory if provided
-  if (!noExternalModel) {
-    copyAndRename(file=modelFile, tempDir=tempDir, newName="model.txt")
-  }
-  # Copy parameters file to export directory if provided
-  if (!is.null(parametersFile) && file.exists(parametersFile)) {
-    copyAndRename(file=parametersFile, tempDir=exportDir, newName="populationParameters.txt")
-  }
   
   # Read mlxtran file and adapt the link to the model file on the fly
   mlxtranStr <- readLines(mlxtran)
@@ -62,39 +48,57 @@ importMonolix <- function(mlxtranFile, modelFile=NULL, parametersFile=NULL, digi
   filePathIndexes <- which(grepl("\\s*file\\s*=\\s*.*", mlxtranStr))
 
   if (length(longitudinalIndex) > 0) {
-    if (noExternalModel) {
-      modelBasename <- "project.mlxtran"
-    } else {
-      modelBasename <- "model.txt"
-    }
+    modelBasename <- "model.txt"
     filePathIndexes <- filePathIndexes[filePathIndexes > longitudinalIndex]
-    modelFilePathIndex <- filePathIndexes[1]
-    mlxtranStr[modelFilePathIndex] <- sprintf("file = '%s'", modelBasename)
+    if (length(filePathIndexes) >0 ) {
+      modelFilePathIndex <- filePathIndexes[1]
+      mlxtranStr[modelFilePathIndex] <- sprintf("file = '%s'", modelBasename)
+    } else {
+      # Bug in monolix2rx: file is needed
+      mlxtranStr <- mlxtranStr %>% append(sprintf("file = '%s'", modelBasename), after=longitudinalIndex)
+    }
   }
   
   # Overwrite the mlxtran file with the modified content
   writeLines(mlxtranStr, mlxtran)
-
-  # Import the mlxtran file
+  
+  # Import the mlxtran file a first time to retrieve the export path
   mlxtranObj <- monolix2rx::mlxtran(file=mlxtran)
-  
-  # Delete dataset file path
-  mlxtranObj$DATAFILE$FILEINFO$FILEINFO$file <- ""
-  
-  # Set the export path to the export directory
-  mlxtranObj$MONOLIX$SETTINGS$GLOBAL$exportpath <- "export"
-  
-  # Error in if (!file.exists(.mlxtran$MODEL$LONGITUDINAL$LONGITUDINAL$file)) {
-  # file is of length 1
+  exportDirName <- mlxtranObj$MONOLIX$SETTINGS$GLOBAL$exportpath
+
+  # Copy model file to temporary directory if provided
   if (noExternalModel) {
-    mlxtranObj$MODEL$LONGITUDINAL$LONGITUDINAL$file <- "project.mlxtran"
+    tmpFile <- tempfile("model", fileext=".txt") # Dummy model file
+    writeLines("", tmpFile)
+    copyAndRename(file=tmpFile, tempDir=tempDir, newName="model.txt")
   } else {
-    mlxtranObj$MODEL$LONGITUDINAL$LONGITUDINAL$file <- "model.txt"
+    copyAndRename(file=modelFile, tempDir=tempDir, newName="model.txt")
+  }
+  
+  # Create export directory
+  exportDir <- file.path(tempDir, exportDirName)
+  if (!dir.exists(exportDir)) {
+    dir.create(exportDir)
+  }
+  
+  # Copy parameters file to export directory if provided
+  if (!is.null(parametersFile) && file.exists(parametersFile)) {
+    copyAndRename(file=parametersFile, tempDir=exportDir, newName="populationParameters.txt")
+  }
+  
+  # Copy parameters file to export directory if provided
+  if (!is.null(covFile) && file.exists(covFile)) {
+    # Create fisher information directory
+    fisherInformationDir <- file.path(exportDir, "FisherInformation")
+    if (!dir.exists(fisherInformationDir)) {
+      dir.create(fisherInformationDir)
+    }
+    copyAndRename(file=covFile, tempDir=fisherInformationDir, newName="covarianceEstimatesSA.txt")
   }
 
   # Convert the mlxtran object to rxode2 model
-  rxmod <- monolix2rx(mlxtranObj, envir=new.env())
-  
+  rxmod <- monolix2rx::monolix2rx(mlxtran, envir=new.env(), thetaMatType="sa")
+
   # Convert the rxode2 model to a functional Campsis model
   model <- importRxode2(rxmod, rem_pop_suffix=TRUE, rem_omega_prefix=TRUE)
   
@@ -121,6 +125,11 @@ importMonolix <- function(mlxtranFile, modelFile=NULL, parametersFile=NULL, digi
         x@value <- signif(x@value, digits=digits)
         return(x)
       })
+  }
+  
+  # Add variance-covariance matrix
+  if (!is.null(rxmod$thetaMat) && nrow(rxmod$thetaMat) > 0) {
+    model@parameters@varcov <- rxmod$thetaMat
   }
 
   return(model)
