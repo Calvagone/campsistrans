87b26a89d4440e0d23cf09c5baf4f1b767e635a8
luyckxn <nicolas.luyckx@calvagone.com>
2025-06-11 09:59:44 +0200

    #92 Save models.

--- tests/testthat/testRxode2Import.R
+++ tests/testthat/testRxode2Import.R
@@ -293,3 +293,172 @@ test_that("thetaMat in rxode2 is automatically converted to a variance-covarianc
   model <- generateModel(rxmod, folder)
   expect_equal(model, read.campsis(nonRegressionRxode2Path(folder)))
 })
+
+test_that("Import of the Mavoglurant PBPK model works as expected", {
+  folder <- "mavoglurant_pbpk"
+  
+  # This model is public, see here: https://nlmixrdevelopment.github.io/nlmixr/articles/mavoglurant.html
+  rxmod <- function() {
+    ini({
+      ##theta=exp(c(1.1, .3, 2, 7.6, .003, .3))
+      lKbBR = 1.1
+      lKbMU = 0.3
+      lKbAD = 2
+      lCLint = 7.6
+      lKbBO = 0.03
+      lKbRB = 0.3
+      eta.LClint ~ 4
+      add.err <- 1
+      prop.err <- 10
+    })
+    model({
+      KbBR = exp(lKbBR)
+      KbMU = exp(lKbMU)
+      KbAD = exp(lKbAD)
+      CLint= exp(lCLint + eta.LClint)
+      KbBO = exp(lKbBO)
+      KbRB = exp(lKbRB)
+      
+      ## Regional blood flows
+      CO  = (187.00*WT^0.81)*60/1000;         # Cardiac output (L/h) from White et al (1968)
+      QHT = 4.0 *CO/100;
+      QBR = 12.0*CO/100;
+      QMU = 17.0*CO/100;
+      QAD = 5.0 *CO/100;
+      QSK = 5.0 *CO/100;
+      QSP = 3.0 *CO/100;
+      QPA = 1.0 *CO/100;
+      QLI = 25.5*CO/100;
+      QST = 1.0 *CO/100;
+      QGU = 14.0*CO/100;
+      QHA = QLI - (QSP + QPA + QST + QGU); # Hepatic artery blood flow
+      QBO = 5.0 *CO/100;
+      QKI = 19.0*CO/100;
+      QRB = CO - (QHT + QBR + QMU + QAD + QSK + QLI + QBO + QKI);
+      QLU = QHT + QBR + QMU + QAD + QSK + QLI + QBO + QKI + QRB;
+      
+      ## Organs' volumes = organs' weights / organs' density
+      VLU = (0.76 *WT/100)/1.051;
+      VHT = (0.47 *WT/100)/1.030;
+      VBR = (2.00 *WT/100)/1.036;
+      VMU = (40.00*WT/100)/1.041;
+      VAD = (21.42*WT/100)/0.916;
+      VSK = (3.71 *WT/100)/1.116;
+      VSP = (0.26 *WT/100)/1.054;
+      VPA = (0.14 *WT/100)/1.045;
+      VLI = (2.57 *WT/100)/1.040;
+      VST = (0.21 *WT/100)/1.050;
+      VGU = (1.44 *WT/100)/1.043;
+      VBO = (14.29*WT/100)/1.990;
+      VKI = (0.44 *WT/100)/1.050;
+      VAB = (2.81 *WT/100)/1.040;
+      VVB = (5.62 *WT/100)/1.040;
+      VRB = (3.86 *WT/100)/1.040;
+      
+      ## Fixed parameters
+      BP = 0.61;      # Blood:plasma partition coefficient
+      fup = 0.028;    # Fraction unbound in plasma
+      fub = fup/BP;   # Fraction unbound in blood
+      
+      KbLU = exp(0.8334);
+      KbHT = exp(1.1205);
+      KbSK = exp(-.5238);
+      KbSP = exp(0.3224);
+      KbPA = exp(0.3224);
+      KbLI = exp(1.7604);
+      KbST = exp(0.3224);
+      KbGU = exp(1.2026);
+      KbKI = exp(1.3171);
+      
+      ##-----------------------------------------
+      S15 = VVB*BP/1000;
+      C15 = Venous_Blood/S15
+      
+      ##-----------------------------------------
+      d/dt(Lungs) = QLU*(Venous_Blood/VVB - Lungs/KbLU/VLU);
+      d/dt(Heart) = QHT*(Arterial_Blood/VAB - Heart/KbHT/VHT);
+      d/dt(Brain) = QBR*(Arterial_Blood/VAB - Brain/KbBR/VBR);
+      d/dt(Muscles) = QMU*(Arterial_Blood/VAB - Muscles/KbMU/VMU);
+      d/dt(Adipose) = QAD*(Arterial_Blood/VAB - Adipose/KbAD/VAD);
+      d/dt(Skin) = QSK*(Arterial_Blood/VAB - Skin/KbSK/VSK);
+      d/dt(Spleen) = QSP*(Arterial_Blood/VAB - Spleen/KbSP/VSP);
+      d/dt(Pancreas) = QPA*(Arterial_Blood/VAB - Pancreas/KbPA/VPA);
+      d/dt(Liver) = QHA*Arterial_Blood/VAB + QSP*Spleen/KbSP/VSP + QPA*Pancreas/KbPA/VPA + QST*Stomach/KbST/VST + QGU*Gut/KbGU/VGU - CLint*fub*Liver/KbLI/VLI - QLI*Liver/KbLI/VLI;
+      d/dt(Stomach) = QST*(Arterial_Blood/VAB - Stomach/KbST/VST);
+      d/dt(Gut) = QGU*(Arterial_Blood/VAB - Gut/KbGU/VGU);
+      d/dt(Bones) = QBO*(Arterial_Blood/VAB - Bones/KbBO/VBO);
+      d/dt(Kidneys) = QKI*(Arterial_Blood/VAB - Kidneys/KbKI/VKI);
+      d/dt(Arterial_Blood) = QLU*(Lungs/KbLU/VLU - Arterial_Blood/VAB);
+      d/dt(Venous_Blood) = QHT*Heart/KbHT/VHT + QBR*Brain/KbBR/VBR + QMU*Muscles/KbMU/VMU + QAD*Adipose/KbAD/VAD + QSK*Skin/KbSK/VSK + QLI*Liver/KbLI/VLI + QBO*Bones/KbBO/VBO + QKI*Kidneys/KbKI/VKI + QRB*Rest_of_Body/KbRB/VRB - QLU*Venous_Blood/VVB;
+      d/dt(Rest_of_Body) = QRB*(Arterial_Blood/VAB - Rest_of_Body/KbRB/VRB);
+      
+      C15 ~ add(add.err) + prop(prop.err)
+    })
+  }
+  
+  model <- generateModel(rxmod, folder)
+  expect_equal(model, read.campsis(nonRegressionRxode2Path(folder)))
+})
+
+test_that("Import of the Friberg Myelosuppression model works as expected", {
+  folder <- "myelosuppression_model"
+  
+  # This model is public, see here: https://nlmixrdevelopment.github.io/nlmixr/articles/wbc.html
+  rxmod <- function() {
+    ini({
+      ## Note that the UI can take expressions
+      ## Also note that these initial estimates should be provided on the log-scale
+      log_CIRC0 <- log(7.21)
+      log_MTT <- log(124)
+      log_SLOPU <- log(28.9)
+      log_GAMMA <- log(0.239)
+      ## Initial estimates should be high for SAEM ETAs
+      eta.CIRC0  ~ .1
+      eta.MTT  ~ .03
+      eta.SLOPU ~ .2
+      ##  Also true for additive error (also ignored in SAEM)
+      prop.err <- 10
+    })
+    model({
+      CIRC0 =  exp(log_CIRC0 + eta.CIRC0)
+      MTT =  exp(log_MTT + eta.MTT)
+      SLOPU =  exp(log_SLOPU + eta.SLOPU)
+      GAMMA = exp(log_GAMMA)
+      
+      # PK parameters from input dataset
+      CL = CLI;
+      V1 = V1I;
+      V2 = V2I;
+      Q = 204;
+      
+      CONC = A_centr/V1;
+      
+      # PD parameters
+      NN = 3;
+      KTR = (NN + 1)/MTT;
+      EDRUG = 1 - SLOPU * CONC;
+      FDBK = (CIRC0 / A_circ)^GAMMA;
+      
+      CIRC = A_circ;
+      
+      A_prol(0) = CIRC0;
+      A_tr1(0) = CIRC0;
+      A_tr2(0) = CIRC0;
+      A_tr3(0) = CIRC0;
+      A_circ(0) = CIRC0;
+      
+      d/dt(A_centr) = A_periph * Q/V2 - A_centr * (CL/V1 + Q/V1);
+      d/dt(A_periph) = A_centr * Q/V1 - A_periph * Q/V2;
+      d/dt(A_prol) = KTR * A_prol * EDRUG * FDBK - KTR * A_prol;
+      d/dt(A_tr1) = KTR * A_prol - KTR * A_tr1;
+      d/dt(A_tr2) = KTR * A_tr1 - KTR * A_tr2;
+      d/dt(A_tr3) = KTR * A_tr2 - KTR * A_tr3;
+      d/dt(A_circ) = KTR * A_tr3 - KTR * A_circ;
+      
+      CIRC ~ prop(prop.err)
+    })
+  }
+  
+  model <- generateModel(rxmod, folder)
+  expect_equal(model, read.campsis(nonRegressionRxode2Path(folder)))
+})
