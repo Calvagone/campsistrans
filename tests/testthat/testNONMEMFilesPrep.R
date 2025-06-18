303a3056fe58976546a5ae7da37aa7c0caa3448d
luyckxn <nicolas.luyckx@calvagone.com>
2025-06-18 11:26:42 +0200

    #95 Comment test.

--- tests/testthat/testNONMEMFilesPrep.R
+++ tests/testthat/testNONMEMFilesPrep.R
@@ -12,16 +12,16 @@ modelPath <- function(folder, filename) {
 
 test_that("Import custom model", {
   
-  test_folder <- file.path("C:/Users/nicolas.luyckx.CALVAGONE/Desktop/Pharmpy/")
-  path <-  file.path(test_folder, "runPKPMPD007" , "runPKPMPD007_QUAL.mod")
-  dataset <- read.csv(file.path(test_folder, "dataset.csv"), header=TRUE, stringsAsFactors=FALSE)
-  campsis <- read.campsis(file.path(test_folder, "Model"))
-  
-  model <- loadCtl(path=path, estimate=TRUE)
-  model_ <- updateCtlForQual(model=model, dataset=dataset, campsis=campsis, variables="CONC")
-  
-  output_folder <- file.path(test_folder, "Export")
-  
-  writeCtl(model=model_, path=file.path(output_folder, "export.mod"), force=TRUE)
+  # test_folder <- file.path("C:/Users/nicolas.luyckx.CALVAGONE/Desktop/Pharmpy/")
+  # path <-  file.path(test_folder, "runPKPMPD007" , "runPKPMPD007_QUAL.mod")
+  # dataset <- read.csv(file.path(test_folder, "dataset.csv"), header=TRUE, stringsAsFactors=FALSE)
+  # campsis <- read.campsis(file.path(test_folder, "Model"))
+  # 
+  # model <- loadCtl(path=path, estimate=TRUE)
+  # model_ <- updateCtlForQual(model=model, dataset=dataset, campsis=campsis, variables="CONC")
+  # 
+  # output_folder <- file.path(test_folder, "Export")
+  # 
+  # writeCtl(model=model_, path=file.path(output_folder, "export.mod"), force=TRUE)
 })
 
