628181332b4ffae143cba07c97c1c622f4690422
luyckxn <nicolas.luyckx@calvagone.com>
2025-07-03 09:42:08 +0200

    #100 Update testVarvovCoversion with nonmem2rx.

--- tests/testthat/testVarcovConversion.R
+++ tests/testthat/testVarcovConversion.R
@@ -4,19 +4,25 @@ library(ggplot2)
 
 context("Working with variance-covariance matrices")
 
-testFolder <- ""
+testFolder <-  file.path(getwd(), test_path())
 overwriteNonRegressionFiles <- FALSE
 
-test_that("Import NONMEM model with cov file and export it to CAMPSIS", {
-  mapping <- mapping(theta=c(CL=1, V1=2, V2=3, Q=4), omega=c(CL=1, V1=2), sigma=c(PROP=1))
-  file <- paste0(testFolder, "parameter_uncertainty/", "example1/", "advan3_trans4.ctl")
+test_that("Import NONMEM model with variance-covariance matrix and export it to Campsis", {
+  
+  modelPath <- file.path(testFolder, "parameter_uncertainty", "example1")
+  ctlFile <- file.path(modelPath, "advan3_trans4.ctl")
+  extFile <- file.path(modelPath, "advan3_trans4.ext")
+  covFile <- file.path(modelPath, "advan3_trans4.cov")
   
   # Import with estimates and full uncertainty
-  object <- importNONMEM(file, mapping=mapping, estimate=TRUE, uncertainty=TRUE, copy_dir=TRUE, rem_rate=TRUE)
-  model <- object %>% export(dest="campsis")
+  object <- importNONMEM2(ctlFile=ctlFile, 
+                          extFile=extFile, 
+                          covFile=covFile)
+  model <- object %>%
+    export(dest="campsis")
   
   # Save CAMPSIS model for non-regression
-  file <- paste0(testFolder, "non_regression/", "parameter_uncertainty/", "example1/")
+  file <- file.path(testFolder, "non_regression", "parameter_uncertainty", "example1")
   if (overwriteNonRegressionFiles) {
     model %>% write(file)
   }
